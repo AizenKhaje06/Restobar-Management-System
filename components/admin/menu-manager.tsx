78ecdf9b19b1b77749231493f8e87eef22b7a5a6
Aizen06 <jhakewihi072@gmail.com>
2026-09-13 22:09:01 +0800

    Admin menu: Reduce card size to 5 per row

--- components/admin/menu-manager.tsx
+++ components/admin/menu-manager.tsx
@@ -236,7 +236,7 @@ export function MenuManager({
                 </CardContent>
               </Card>
             ) : (
-              <div className="grid gap-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
                 {filteredItems.map((item) => (
                   <ItemCard
                     key={item.id}
